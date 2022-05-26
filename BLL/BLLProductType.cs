56fe557a22e679090b17a6c5095fbff7020568e7
codewithpriya-522 <priya747730@gmail.com>
2022-05-26 15:17:09 +0530

    Add project files.

---
+++ BLL/BLLProductType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+
+namespace BLL
+{
+    public class BLLProductType
+    {
+        string _productTypeBLL;
+        //ata local value sudhu ai class e use hobe or e akta prop banalam niche jeta onno class theke access hobe ata subidhe maintain korte
+        public string ProductTypeBLL { get => _productTypeBLL; set => _productTypeBLL = value; }//aita onno class e use hobe dekh public o ata 2 to jinis e aktai veriable ke point korche
+
+        public int BllInsertProductType()
+        {
+            DALProductType productType = new DALProductType();
+            productType.ProductType = _productTypeBLL;
+            //Arom nam dile nijei confuse hoye jabi konta ki
+            int roeEffected=productType.InsertProductType();
+            return roeEffected;
+            //done bujte parli ki changes korlam ui teo same kaj hobe
+        }
+    }
+}
---
+++ DAL/DALProductType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    public class DALProductType
+    {
+        //avabe proc likhle kaj korbe na 
+        string _productType;
+        public string _ProductTypeName{ get; set; }
+        public string ProductType { get => _productType; set => _productType = value; }
+
+        public int InsertProductType()
+        {
+            string CS=ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection sqlConnection = null;
+            int RowsEffected = 0;
+            try
+            {
+                sqlConnection = new SqlConnection(CS);
+                SqlCommand sqlCommand = new SqlCommand("spInsertProductTypeData", sqlConnection);
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@ProductTypeName", _productType);
+                sqlConnection.Open();
+                RowsEffected= sqlCommand.ExecuteNonQuery();
+                return RowsEffected;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
